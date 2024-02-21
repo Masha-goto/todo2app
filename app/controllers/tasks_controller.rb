f6c5f5b18dfabdd738794aaca9f57b16cbd99f83
masahiro <oosaki.production@gmail.com>
2024-02-21 16:18:41 +0900

    タイトルとボードの概要を入力ができる

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,4 +1,16 @@
 class TasksController < ApplicationController
 	def index
+		@tasks = Task.all
+		@task = Task.new
+	end
+
+	def show
+		@task = Task.find(params[:id])
+	end
+
+	def new
+	end
+
+	def create
 	end
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,6 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+Task.create({title: 'todlist', content: 'ああああああああああああ'})
+Task.create({title: 'あああああああああああ', content: 'いいいいいいいいいいいい'})
