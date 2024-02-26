438fbf5913622711b692a4b4bd099c1f4f765d48
masahiro <oosaki.production@gmail.com>
2024-02-26 16:44:24 +0900

    記事表示、投稿機能、更新、削除機能追加 flash, Strong Parameter, beforeアクション,backbutton実装

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,16 +1,51 @@
 class TasksController < ApplicationController
+	before_action :set_task, only: [:show, :edit, :update]
+
 	def index
 		@tasks = Task.all
-		@task = Task.new
 	end
 
 	def show
-		@task = Task.find(params[:id])
 	end
 
 	def new
+		@task = Task.new
 	end
 
 	def create
+		@task = Task.new(task_params)
+		if @task.save
+			redirect_to task_path(@task), notice: 'Saved in.'
+		else
+			flash.now[:error] = 'Failed to save.'
+			render :new
+		end
 	end
+
+	def edit
+	end
+
+	def update
+		if @task.update(task_params)
+			redirect_to task_path(@task), notice: 'I was able to change it.'
+		else
+			flash.now[:error] = 'Could not change.'
+			render :edit
+		end
+	end
+
+	def destroy
+		article = Task.find(params[:id])
+		article.destroy!
+		redirect_to root_path, notice: 'Successfully deleted.'
+	end
+
+	private
+	def task_params
+		params.require(:task).permit(:title, :content)
+	end
+
+	def set_task
+    @task = Task.find(params[:id])
+  end
 end
